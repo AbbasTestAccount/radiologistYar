56be9dc27e3e59764920e5bdce4e9704ee87a1dc
AbbasTestAccount <abbasfake5@gmail.com>
2024-07-30 04:29:32 +0330

    add feature to show pdf files

---
+++ src/component/Pages/LearningPdfStyle.js
@@ -0,0 +1,30 @@
+import './Learning.css'
+import TreeView from '../TreeView';
+import React, { useEffect } from "react";
+import Test from './Test2';
+import SplitPane from "react-split-pane";
+
+
+
+function Learning(props) {
+  useEffect(()=>{
+    const topBar = document.getElementById('topBar')
+    const topBarRect = topBar.getBoundingClientRect()
+    document.getElementsByClassName('Pane')[0].style.height = `calc(100vh - ${topBarRect.y + topBar.offsetHeight}px)`
+    document.getElementsByClassName('Pane')[1].style.height = `calc(100vh - ${topBarRect.y + topBar.offsetHeight}px)`
+    document.getElementsByClassName('SplitPane')[0].style.height = ''
+  }, [props.browserHeight])
+
+  return (
+    <div id='learning-page' className="content-below-toolBar">
+      <SplitPane split="vertical" minSize={200} defaultSize={300} maxSize={600}>
+        <TreeView></TreeView>
+        <div className='learning-content'>
+          <Test></Test>
+        </div>
+      </SplitPane>
+    </div>
+  );
+}
+
+export default Learning;
